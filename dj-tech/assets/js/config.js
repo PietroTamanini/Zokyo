7cf1ea4a66f9a8a3743f924aee2895ceb82d3113
Pietro Tamanini <pietroramostamanini2010@gmail.com>
2026-09-11 10:54:58 -0300

    revert dj tech public site redesign

--- dj-tech/assets/js/config.js
+++ dj-tech/assets/js/config.js
@@ -1,5 +1,5 @@
 window.DJTECH_SITE = {
-  apiBase: 'https://os.djtechinfo.com.br',
+  apiBase: '',
   whatsapp: '5547991425273',
   phoneLabel: '(47) 99142-5273',
   address: 'Rua Pavo, 217 - Jardim Paraiso, Joinville - SC',
